6d7190787f2320f6bbab193b8412bd0c6f077241
EEPSU <EEPSU@wv10ld000272.belwired.net>
2020-05-19 16:22:51 +0200

    commit test

---
+++ Peeters_Suzy_GamesJDBC/src/test/resources/GameServiceTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+
+import be.belfius.GamesJDBC.services.GameService;
+
+public class GameServiceTest {
+	 
+	    public GameService gameService = new GameService();
+	    
+	    @Test
+	    public void testGetAllDIff() {
+	        Assert.assertFalse(gameService.getAllDiff().isEmpty());
+	    }
+	    
+	    
+	}
